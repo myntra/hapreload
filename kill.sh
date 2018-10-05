e0cfa95ef1951bb03ca2ea20b2fb9523b3b634b6
Sreehari Mohan <sreehari.mohan@myntra.com>
2018-10-05 18:45:21 +0530

    using the right space separated value

--- kill.sh
+++ kill.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -efo comm,pid,args | grep haproxy.cfg | grep -v grep | awk '{print $1}' | xargs -I {} kill -9 {}
+ps -efo comm,pid,args | grep haproxy.cfg | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
