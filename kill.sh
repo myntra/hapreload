1b4593c723b8928bcd4186e4601bb561028e6807
sumanth177 <sumanthkumar177@gmail.com>
2018-10-17 17:21:01 +0530

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -efo comm,pid,args | grep haproxy.cfg | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+ps -efo comm,pid,args | grep haproxy.cfg | grep -v grep | awk '{print $2}' | sed -e "s/haproxy//" | xargs -I {} kill -9 {}
