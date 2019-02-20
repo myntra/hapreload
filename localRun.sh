d7572d4f389c65b1b1f6b205f14a1cabd3b38714
Sreehari Mohan <sreehari.mohan@myntra.com>
2019-02-20 18:45:00 +0530

    upgrade to haproxy 1.9.4

--- localRun.sh
+++ localRun.sh
@@ -1,4 +1,4 @@
 docker rm -f hap
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build .
 docker build -t hapreload .
-docker run -d -v /Users/sreehari.mohan/haproxy:/usr/local/etc/haproxy -p 8000:80 -p 443:443 -p 34015:34015 --name hap hapreload
+docker run -d -v $HOME/haproxy:/usr/local/etc/haproxy -p 8888:80 -p 443:443 -p 34015:34015 --name hap hapreload
