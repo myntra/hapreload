b02ce905f02249fad6cc70f5f6c8a7edcd8a6a8a
sumanth177 <sumanthkumar177@gmail.com>
2019-04-09 17:39:08 +0530

    Kill Ha proxy is not working as it is not listing the process which is not attached to the shell

--- kill.sh
+++ kill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PID_LIST=`ps -efo comm,pid,args | grep haproxy.cfg | grep -v grep | awk '{print $2}'`
+PID_LIST=`ps -eo comm,pid,args | grep haproxy.cfg | grep -v grep | awk '{print $2}'`
 FINAL_PID=`echo $PID_LIST | sed -E 's/haproxy//g'`
 kill -9  $FINAL_PID
 exit 0
